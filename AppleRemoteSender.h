26a61a74e28a928d3cfe36acbd6dce96b1c519f2
Julien Boriasse <julienboriasse@users.noreply.github.com>
2013-12-18 09:33:28 -0500

    Update AppleRemoteSender.h
    Replace #include "WProgram.h" per #include "Arduino.h"

--- AppleRemoteSender.h
+++ AppleRemoteSender.h
@@ -58,7 +58,7 @@ The last two bytes are 0x87EE - which identifies this as an Apple device.
 
 #include <inttypes.h>
 #include <avr/io.h>
-#include "WProgram.h"
+#include "Arduino.h"
 
 //suffix that all remotes use.
 #define APPLE_ID 0x87EE
